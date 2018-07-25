bc992d86394d72d553a4f2971c2b202ba9c5ae40
lapis-zero09 <lapis-zero09@klis.tsukuba.ac.jp>
2018-07-26 01:00:25 +0900

    ch03 ex03

--- ch03/ex03/main_test.go
+++ ch03/ex03/main_test.go
@@ -1,10 +1,10 @@
 package main
 
 func ExampleCheckInf() {
-	// checkInfAndPrint([]float64{0, 0, 0, 0, 0, 0, 0, math.NaN()})
-	// checkInfAndPrint([]float64{0, 0, 0, 0, 0, 0, 0, math.Inf(0)})
-	checkInfAndPrint([]float64{12.423, 12.423, 12.423, 12.423, 12.423, 12.423, 12.423, 12.423})
+	// checkInfAndPrint([]float64{0, 0, 0, 0, 0, 0, 0, math.NaN()}, 255, 255)
+	// checkInfAndPrint([]float64{0, 0, 0, 0, 0, 0, 0, math.Inf(0)}, 255, 255)
+	checkInfAndPrint([]float64{2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4}, 255, 255)
 
 	// Output:
-	// <polygon points='12.423,12.423 12.423,12.423 12.423,12.423 12.423,12.423'/>
+	// <polygon points='2.4,2.4 2.4,2.4 2.4,2.4 2.4,2.4' fill='rgba(255,0,255,0.8)' stloke='rgba(255,0,255,0.8)'/>
 }
