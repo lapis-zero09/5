4cc744dd6611df61edc890c1a11d089d08998f75
河野　晋策 <shinsaku_kono@r.recruit.co.jp>
2018-10-23 20:44:02 +0900

    ch08 ex09

---
+++ ch08/ex08/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	input := bufio.NewScanner(c)
+	ch := make(chan string)
+	go func() {
+		for input.Scan() {
+			ch <- input.Text()
+		}
+	}()
+
+	for {
+		select {
+		case line, ok := <-ch:
+			if !ok {
+				return
+			}
+			go echo(c, line, 1*time.Second)
+		case <-time.After(10 * time.Second):
+			return
+		}
+	}
+}
