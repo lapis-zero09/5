058053608491ebac377ce8793f976c92488c0a09
lapis-zero09 <lapis-zero09@klis.tsukuba.ac.jp>
2018-12-17 00:22:26 +0900

    ch12 ex06

--- ch12/ex06/sexpr_test.go
+++ ch12/ex06/sexpr_test.go
@@ -90,97 +90,49 @@ func TestMarshalInterface(t *testing.T) {
 	}
 }
 
-func TestMarshalIndent(t *testing.T) {
+func TestMarshal(t *testing.T) {
 	type Movie struct {
 		Title, Subtitle string
 		Year            int
-		Actor           map[string]string
 		Oscars          []string
 		Sequel          *string
 	}
-	strangelove := Movie{
-		Title:    "Dr. Strangelove",
-		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
-		Year:     1964,
-		Actor: map[string]string{
-			"Dr. Strangelove":            "Peter Sellers",
-			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+
+	var ts = []struct {
+		m        Movie
+		expected string // expected error from Parse/Check or result from Eval
+	}{
+		{
+			m: Movie{
+				Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+				Year:     1964,
+				Oscars: []string{
+					"Best Actor (Nomin.)",
+					"Best Adapted Screenplay (Nomin.)",
+				},
+			},
+			expected: "( (Subtitle \"How I Learned to Stop Worrying and Love the Bomb\") (Year 1964) (Oscars (\"Best Actor (Nomin.)\" \"Best Adapted Screenplay (Nomin.)\")))",
 		},
-		Oscars: []string{
-			"Best Actor (Nomin.)",
-			"Best Adapted Screenplay (Nomin.)",
+		{
+			m: Movie{
+				Title: "Dr. Strangelove",
+				Oscars: []string{
+					"Best Actor (Nomin.)",
+					"Best Adapted Screenplay (Nomin.)",
+				},
+			},
+			expected: "((Title \"Dr. Strangelove\") (Oscars (\"Best Actor (Nomin.)\" \"Best Adapted Screenplay (Nomin.)\")))",
 		},
 	}
 
-	b, err := MarshalIndent(strangelove)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, tc := range ts {
+		b, err := Marshal(tc.m)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	expected := `((Title "Dr. Strangelove")
- (Subtitle "How I Learned to Stop Worrying and Love the Bomb") (Year 1964)
- (Actor
-  (("Dr. Strangelove" "Peter Sellers")
-   ("Grp. Capt. Lionel Mandrake" "Peter Sellers")))
- (Oscars ("Best Actor (Nomin.)" "Best Adapted Screenplay (Nomin.)"))
- (Sequel nil))`
-	if got := string(b); got != expected {
-		t.Errorf("unexpected bytes. \nexpected: \n%v\nbut got: \n%v", expected, got)
+		if got := string(b); got != tc.expected {
+			t.Errorf("unexpected bytes. \nexpected: \n%v\nbut got: \n%v", tc.expected, got)
+		}
 	}
-
 }
-
-// func Test(t *testing.T) {
-// 	type Movie struct {
-// 		Title, Subtitle string
-// 		Year            int
-// 		Actor           map[string]string
-// 		Oscars          []string
-// 		Sequel          *string
-// 	}
-// 	strangelove := Movie{
-// 		Title:    "Dr. Strangelove",
-// 		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
-// 		Year:     1964,
-// 		Actor: map[string]string{
-// 			"Dr. Strangelove":            "Peter Sellers",
-// 			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
-// 			"Pres. Merkin Muffley":       "Peter Sellers",
-// 			"Gen. Buck Turgidson":        "George C. Scott",
-// 			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
-// 			`Maj. T.J. "King" Kong`:      "Slim Pickens",
-// 		},
-// 		Oscars: []string{
-// 			"Best Actor (Nomin.)",
-// 			"Best Adapted Screenplay (Nomin.)",
-// 			"Best Director (Nomin.)",
-// 			"Best Picture (Nomin.)",
-// 		},
-// 	}
-
-// 	// Encode it
-// 	data, err := Marshal(strangelove)
-// 	if err != nil {
-// 		t.Fatalf("Marshal failed: %v", err)
-// 	}
-// 	t.Logf("Marshal() = %s\n", data)
-
-// 	// Decode it
-// 	var movie Movie
-// 	if err := Unmarshal(data, &movie); err != nil {
-// 		t.Fatalf("Unmarshal failed: %v", err)
-// 	}
-// 	t.Logf("Unmarshal() = %+v\n", movie)
-
-// 	// Check equality.
-// 	if !reflect.DeepEqual(movie, strangelove) {
-// 		t.Fatal("not equal")
-// 	}
-
-// 	// Pretty-print it:
-// 	data, err = MarshalIndent(strangelove)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	t.Logf("MarshalIdent() = %s\n", data)
-// }
